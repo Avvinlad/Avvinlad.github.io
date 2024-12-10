ad44f51496d99b8e81c6a132b37efbe399a18f1e
Avin Lad <avinlad@gmail.com>
2024-12-10 01:51:08 -0500

    fixed Experience section for mobile

--- src/widgets/Experience.tsx
+++ src/widgets/Experience.tsx
@@ -9,12 +9,12 @@ export const metadata: Metadata = {
 
 export default function Experience() {
     return (
-        <div id="experience">
+        <div id="experience" className="flex flex-col px-8 justify-center mx-auto">
             <div>
                 <h2 className="text-3xl mb-1 font-extrabold">Work</h2>
                 <h3 className="text-lg">What I Do Best.</h3>
             </div>
-            <div className="flex flex-row w-4/5 my-6 mx-auto">
+            <div className="flex flex-row my-6 mx-auto lg:w-11/12">
                 <Accordion className="w-full" type="single" defaultValue="item-1">
                     {EXPERIENCE.map((exp, index) => {
                         return (
@@ -45,8 +45,8 @@ const EXPERIENCE = [
         name: "Brock Solutions",
         imagePath: "brock_solutions.png",
         description: (
-            <div className="mx-16">
-                <p className="text-xl">Software Developer</p>
+            <div className="lg:mx-16">
+                <p className="text-lg lg:text-xl">Software Developer</p>
                 <p className="text-md">September 2022 - Present</p>
                 <ul className="grid gap-y-2 my-3 list-disc mx-4">
                     <li>
@@ -69,8 +69,8 @@ const EXPERIENCE = [
         name: "NGen Canada",
         imagePath: "ngen_canada.png",
         description: (
-            <div className="mx-16">
-                <p className="text-xl">Data Analyst</p>
+            <div className="lg:mx-16">
+                <p className="text-lg lg:text-xl">Data Analyst</p>
                 <p className="text-md ">January 2022 - April 2022</p>
                 <ul className="grid gap-y-2 my-3 list-disc mx-4">
                     <li>
@@ -93,8 +93,8 @@ const EXPERIENCE = [
         name: "LogiSense",
         imagePath: "logisense.png",
         description: (
-            <div className="mx-16">
-                <p className="text-xl">Software Developer</p>
+            <div className="lg:mx-16">
+                <p className="text-lg lg:text-xl">Software Developer</p>
                 <p className="text-md ">May 2021 - August 2021</p>
                 <ul className="grid gap-y-2 my-3 list-disc mx-4">
                     <li>
@@ -110,8 +110,8 @@ const EXPERIENCE = [
         name: "SoCast Digital",
         imagePath: "socast.png",
         description: (
-            <div className="mx-16">
-                <p className="text-xl">Junior Developer</p>
+            <div className="lg:mx-16">
+                <p className="text-lg lg:text-xl">Junior Developer</p>
                 <p className="text-md ">September 2020 - December 2020</p>
                 <ul className="grid gap-y-2 my-3 list-disc mx-4">
                     <li>Reduced reported client tickets by 30% from fixing bugs and making enhancements using PHP.</li>
@@ -124,8 +124,8 @@ const EXPERIENCE = [
         name: "Hippos Software",
         imagePath: "hippos_software.png",
         description: (
-            <div className="mx-16">
-                <p className="text-xl">Junior Software Developer</p>
+            <div className="lg:mx-16">
+                <p className="text-lg lg:text-xl">Junior Software Developer</p>
                 <p className="text-md ">May 2019 - August 2020</p>
                 <ul className="grid gap-y-2 my-3 list-disc mx-4">
                     <li>
